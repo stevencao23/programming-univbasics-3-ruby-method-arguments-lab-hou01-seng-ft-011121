54c742699c05444bc42c0b08c6859c3277db4d1a
steven cao <stevencao23@gmail.com>
2020-11-08 20:27:56 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,17 @@
+def introduction (name)
+  puts "Hi!, my name is #{name}."
+end
+
+introduction "steven"
+
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+introduction_with_language "steven", "ruby"
+
+def introduction_with_language_optional (name, language = "ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+introduction_with_language_optional ("steven")
