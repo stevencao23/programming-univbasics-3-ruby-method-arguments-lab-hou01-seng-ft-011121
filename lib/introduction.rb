e099176de08c7793937ba13ec47ad8aaa7b0efdb
steven cao <stevencao23@gmail.com>
2020-11-08 20:18:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,6 +12,9 @@ end
 introduction_with_language "steven", "ruby"
 
 def introduction_with_language_optional (name, language = "ruby")
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+  
+introduction_with_language_optional ("steven")
   
 
 
